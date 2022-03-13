d5a124c8b68de842cc02602c2be7b2acd6d5bb3b
Antosser <antosser@antosser.xyz>
2022-03-13 23:05:44 +0100

    1000ms delay -->500ms delay

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $('#gen').click(() => {
     console.log('rybnzdxhrtdxnyb');
-    var scriptInside = btoa("if(new Date()>new Date('{{TIME}}'))setTimeout(()=>document.body.innerHTML='time expired', 1000);".replace('{{TIME}}', $('#in').val()));
+    var scriptInside = btoa("if(new Date()>new Date('{{TIME}}'))setTimeout(()=>document.body.innerHTML='time expired', 500);".replace('{{TIME}}', $('#in').val()));
     scriptInside = `eval(atob('${scriptInside}'))`;
     var result = `<script defer>${scriptInside}</script>`;
     $('#out').val(result);
